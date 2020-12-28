ea0fb3ee794e908be33eafa5c88f5595e1daed78
Riw Royer <ne.free532@gmail.com>
2020-12-28 22:12:11 +0300

    Описана реализация класса Button

--- Mosaic/Button.cpp
+++ Mosaic/Button.cpp
@@ -1,1 +1,9 @@
 #include "Button.h"
+
+bool Button::isPress(int16_t x, int16_t y)
+{
+	if ((x >= xBegin) && (y >= yBegin) && (x <= xEnd) && (y <= yEnd))
+		return true;
+
+	return false;
+}
--- Mosaic/Button.h
+++ Mosaic/Button.h
@@ -1,8 +1,19 @@
 #ifndef _MY_BUTTON_H_
 #define _MY_BUTTON_H_
 
+#include "Library.h"
+
 class Button
 {
+private:
+	const int16_t xBegin;
+	const int16_t yBegin;
+	const int16_t xEnd;
+	const int16_t yEnd;
+
+public:
+	Button(int16_t xB, int16_t yB, int16_t xE, int16_t yE) : xBegin(xB), yBegin(yB), xEnd(xE), yEnd(yE) {};
+	bool isPress(int16_t x, int16_t y);
 };
 
 #endif // !_MY_BUTTON_H_

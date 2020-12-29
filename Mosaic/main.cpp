844a6b15d69e45dec0730160964a4477f457e9f9
Riw Royer <ne.free532@gmail.com>
2020-12-30 01:05:15 +0300

    Добавлена пояснительная записка

--- Mosaic/main.cpp
+++ Mosaic/main.cpp
@@ -4,6 +4,10 @@
 int16_t main()
 {
 	setlocale(LC_ALL, "rus");
+
+	std::cout << "Для добавления свои изображений:\n\t-> Скопируйте нужный вам файл в расширении .JPG в папку resources,"
+		"\n\tпредварительно переименовав его в Image[Номер от 1 до 4]." << std::endl;
+
 	GamePazzle myGame;
 
 	return myGame.startGame();
